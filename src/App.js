58ef54d84375db2f7ab8febb5f7651df180402c0
shelleychenn <shelleychen322@gmail.com>
2020-08-12 22:16:29 -0400

    Import board component in app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import Board from './components/Board';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="tic-tac-toe-app">
-        <div className="app-title">tic-tac-toe</div>
-        <div className="game-board">
-          <Board />
-        </div>
+const App = () => {
+  return (
+    <div className="tic-tac-toe-game">
+      <div className="game-title">tic-tac-toe</div>
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default hot(App);
